24343d29a4d9c5197713b532838f0394a81769e6
BuildTools <unconfigured@null.spigotmc.org>
2023-05-15 21:02:03 +0200

    Exported data table from 'script.js'

---
+++ data/firstData.js
@@ -0,0 +1,93 @@
+export const data = [
+  {
+    title: "Day of the Dragon",
+    author: "Richard A. Knaak",
+    quantity: 10,
+    unit_price: 9,
+    total_value: null,
+  },
+  {
+    title: "A Wizard of Earthsea",
+    author: "Ursula K. Le Guin",
+    quantity: null,
+    unit_price: 10,
+    total_value: 40,
+  },
+  {
+    title: "Homeland",
+    author: "Robert A. Salvatore",
+    quantity: 8,
+    unit_price: null,
+    total_value: 96,
+  },
+  {
+    title: "Canticle",
+    author: "Robert A. Salvatore",
+    quantity: 13,
+    unit_price: 23,
+    total_value: null,
+  },
+  {
+    title: "Gamedec. Granica rzeczywistości",
+    author: "Marcin Przybyłek",
+    quantity: null,
+    unit_price: 25,
+    total_value: 50,
+  },
+  {
+    title: "The Night Has Come",
+    author: "Stephen King",
+    quantity: 30,
+    unit_price: null,
+    total_value: 900,
+  },
+  {
+    title: "The Sphinx",
+    author: "Graham Masterton",
+    quantity: 3,
+    unit_price: null,
+    total_value: 300,
+  },
+  {
+    title: "Charnel House",
+    author: "Graham Masterton",
+    quantity: null,
+    unit_price: 20,
+    total_value: 60,
+  },
+  {
+    title: "The Devils of D-Day",
+    author: "Graham Masterton",
+    quantity: 10,
+    unit_price: 16,
+    total_value: null,
+  },
+];
+
+export const metadata = [
+  {
+    id: "title",
+    type: "string",
+    label: "Title",
+  },
+  {
+    id: "author",
+    type: "string",
+    label: "Author",
+  },
+  {
+    id: "quantity",
+    type: "number",
+    label: "Quantity",
+  },
+  {
+    id: "unit_price",
+    type: "number",
+    label: "Unit price",
+  },
+  {
+    id: "total_value",
+    type: "number",
+    label: "Total (Quantity * Unit price)",
+  },
+];
---
+++ data/secondData.js
@@ -0,0 +1,95 @@
+// additionalDataFromBooksDB
+export const data = [
+  {
+    title: "Day of the Dragon",
+    author: "Richard A. Knaak",
+    genre: "fantasy",
+    pages: 378,
+    rating: 3.81,
+  },
+  {
+    title: "A Wizard of Earthsea",
+    author: "Ursula K. Le Guin",
+    genre: "fantasy",
+    pages: 183,
+    rating: 4.01,
+  },
+  {
+    title: "Homeland",
+    author: "Robert A. Salvatore",
+    genre: "fantasy",
+    pages: 343,
+    rating: 4.26,
+  },
+  {
+    title: "Canticle",
+    author: "Robert A. Salvatore",
+    genre: "fantasy",
+    pages: 320,
+    rating: 4.03,
+  },
+  {
+    title: "Gamedec. Granica rzeczywistości",
+    author: "Marcin Przybyłek",
+    genre: "cyberpunk",
+    pages: 364,
+    rating: 3.89,
+  },
+  {
+    title: "The Night Has Come",
+    author: "Stephen King",
+    genre: "post apocalyptic",
+    pages: 186,
+    rating: 4.55,
+  },
+  {
+    title: "The Sphinx",
+    author: "Graham Masterton",
+    genre: "horror",
+    pages: 207,
+    rating: 3.14,
+  },
+  {
+    title: "Charnel House",
+    author: "Graham Masterton",
+    genre: "horror",
+    pages: 123,
+    rating: 3.61,
+  },
+  {
+    title: "The Devils of D-Day",
+    author: "Graham Masterton",
+    genre: "horror",
+    pages: 243,
+    rating: "3.62",
+  },
+];
+
+// additionalMetadataFromBooksDB
+const metadata = [
+  {
+    id: "title",
+    type: "string",
+    label: "Title",
+  },
+  {
+    id: "author",
+    type: "string",
+    label: "Author",
+  },
+  {
+    id: "genre",
+    type: "string",
+    label: "Genre",
+  },
+  {
+    id: "pages",
+    type: "number",
+    label: "Pages",
+  },
+  {
+    id: "rating",
+    type: "number",
+    label: "Rating",
+  },
+];
